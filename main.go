8b65d5fdc0a14199051bade25a1312f811f7bb1b
Vikas Adiwal <adiwalv@gmail.com>
2020-02-13 22:18:48 +0530

    added a few colors to spice things up

--- main.go
+++ main.go
@@ -23,6 +23,31 @@ var color = map[string]string{
 	"green": "\x1b[32;1m",
 	"reset": "\x1b[0m"}
 
+var (
+	Info = Magenta
+	Warn = Yellow
+	Fata = Red
+)
+
+var (
+	Black   = Color("\033[1;30m%s\033[0m")
+	Red     = Color("\033[1;31m%s\033[0m")
+	Green   = Color("\033[1;32m%s\033[0m")
+	Yellow  = Color("\033[1;33m%s\033[0m")
+	Purple  = Color("\033[1;34m%s\033[0m")
+	Magenta = Color("\033[1;35m%s\033[0m")
+	Teal    = Color("\033[1;36m%s\033[0m")
+	White   = Color("\033[1;37m%s\033[0m")
+)
+
+func Color(colorString string) func(...interface{}) string {
+	sprint := func(args ...interface{}) string {
+		return fmt.Sprintf(colorString,
+			fmt.Sprint(args...))
+	}
+	return sprint
+}
+
 type connections struct {
 	id         string
 	name       string
@@ -217,7 +242,7 @@ func main() {
 		os.Exit(1)
 	}
 	profileNames := getAllProfiles(&gt)
-	fmt.Println("List of VPNS:", profileNames)
+	fmt.Println(Info("List of VPNS:", profileNames))
 
 	if *l {
 		listConnections(&gt)
